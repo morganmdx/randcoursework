a7bcab2277213afd69edf0de35517c57e15a58e4
morganmdx <mm4274@live.mdx.ac.uk>
2024-03-06 22:51:14 +0000

    Import libraries and added baseline comments

--- calculator.py
+++ calculator.py
@@ -1,2 +1,14 @@
+# import libraries
 import threading
 import tkinter
+
+#creating a basic calculator in Python, with a fully functional GUI
+
+# use any library they see fit to help implement the GUI
+
+#Multiplication/Division x /
+# Subtraction/Additional - +
+# Equal/Clear = C
+# Should operate fully with precision, there should be no rounding or missing decimal places
+
+# Multi-threading
