51497cb8ab14303d908b17fcd9b08fbb5f8d05bf
morganmdx <mm4274@live.mdx.ac.uk>
2024-03-06 23:00:53 +0000

    Define new functions and added comments

--- calculator.py
+++ calculator.py
@@ -7,24 +7,37 @@ import tkinter
 # use any library they see fit to help implement the GUI
 
 #Multiplication/Division x /
+def Multiplication():
+    # multiply numbers
+    print()
+
+def Addition():
+    # add numbers together
+    print()
+
+def Subtraction():
+    # when user selects this subtract
+    print()
+
+def Division():
+    # when user presses this divide
+    print()
+
+def Equals():
+    # when user presses equals execute this
+    print()
+
+def Clear():
+    # clear the contents of any user input
+    print()
+
+
+# Our criteria as follows:
 # Subtraction/Additional - +
 # Equal/Clear = C
 # Should operate fully with precision, there should be no rounding or missing decimal places
 
-# Multi-threading
-
-
-
-
-
-
-
-
-
-
-
-
-
+# Multi-threading 
 
 if __name__ == "__main__":
     run_calculator()
